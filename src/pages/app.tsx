b617c26f2f07ed21d91e20335a97375c77691247
androso <androso421@gmail.com>
2022-09-14 07:47:44 -0600

    Adding tailwind and daisyui

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -2,7 +2,7 @@
 export default function App() {
 	return (
 		<div>
-			<h1>App</h1>
+			<h1 className="text-xl text-red-800">App</h1>
 			<p>
 				This is a simple example of a React component.
 			</p>
