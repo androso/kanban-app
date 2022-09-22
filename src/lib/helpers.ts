1f1ec3d7a77d14eb6f0948e6f13462fce96a8b59
androso <androso421@gmail.com>
2022-09-21 19:25:52 -0600

    Handling errors correctly in client

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,35 +1,32 @@
-const API = process.env.NEXT_PUBLIC_API_BASE_URL
+const API = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const client = async (endpoint: RequestInfo, options?: RequestInit) => {
 	const { body, ...customConfig } = options ?? {};
-	
-	const headers = { 'Content-Type': 'application/json' };
+	const headers = { "Content-Type": "application/json" };
 
 	let config: RequestInit = {
-		method: body ? 'POST' : 'GET',
+		method: body ? "POST" : "GET",
 		...customConfig,
 		headers: {
 			...headers,
-			...customConfig.headers
+			...customConfig.headers,
 		},
-
-	}
+	};
 	if (body) {
 		config.body = JSON.stringify(body);
 	}
 	let dataReturned;
-	try {
-		const response = await window.fetch(`${API}${endpoint}`, config);
-		console.log(response);
-		dataReturned = await response.json();
-		
-		if (response.ok) {
-			return dataReturned;
-		}
-		throw new Error(response.statusText);
-	} catch (e: unknown) {
-		if (e instanceof Error) {
-			return Promise.reject(e.message);
-		}
 
+	const response = await window.fetch(`${API}${endpoint}`, config);
+	if (response.status === 401) {
+		window.location.assign(window.location.pathname);
+		return;
 	}
-}
+
+	if (response.ok) {
+		dataReturned = await response.json();
+		return dataReturned;
+	} else {
+		const errorMessage = (await response.json()).message;
+		return Promise.reject(new Error(errorMessage));
+	}
+};
