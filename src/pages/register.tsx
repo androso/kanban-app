b73841cdf6be7ab5120a0624d4a28f74b95db007
androso <androso421@gmail.com>
2022-09-21 19:28:39 -0600

    showing errors in form when registering

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,15 +1,36 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
-import { client } from '../lib/helpers';
+import { client } from "../lib/helpers";
 
-export default function register() {
+export default function Register() {
+	const router = useRouter();
+	const [registerError, setRegisterError] = React.useState<null | Error>(null);
+
 	const registerUser = async (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
-		const $email = event.target?.email;
-		const $password = event.target?.password;
-		const body = { email: $email.value, password: $password.value }
-		const result = await client('/register', { body });
-	}
+
+		const $email = event.currentTarget.elements[0] as HTMLInputElement;
+		const $name = event.currentTarget.elements[1] as HTMLInputElement;
+		const $password = event.currentTarget.elements[2] as HTMLInputElement;
+		const body = {
+			email: $email.value,
+			password: $password.value,
+			name: $name.value,
+		};
+		try {
+			const result = await client("/register", {
+				body: body as unknown as BodyInit,
+			});
+			if (result.status === 201) {
+				router.push("/login", undefined, { shallow: true });
+			}
+		} catch (error) {
+			if (error instanceof Error) {
+				setRegisterError(error);
+			}
+		}
+	};
 	return (
 		<div className="w-full h-[100vh] flex justify-center items-center">
 			<div className="card card-normal bg-base-300 shadow-lg max-w-lg">
@@ -17,6 +38,11 @@ export default function register() {
 					<div className="card-title">
 						<h1>Create a kanban account</h1>
 					</div>
+
+					<span className="label-text text-error">
+						{registerError?.message}
+					</span>
+
 					<div className="form-control w-full max-w-xs">
 						<label htmlFor="username" className="label">
 							<span className="label-text">Email</span>
@@ -26,6 +52,19 @@ export default function register() {
 							name="email"
 							placeholder="type here"
 							id="username"
+							className="input input-bordered"
+							required
+						/>
+					</div>
+					<div className="form-control w-full max-w-xs">
+						<label htmlFor="name" className="label">
+							<span className="label-text">Name</span>
+						</label>
+						<input
+							type="text"
+							name="name"
+							placeholder="type here"
+							id="name"
 							className="input input-bordered"
 							required
 						/>
