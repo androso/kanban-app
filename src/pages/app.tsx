bc43baa0c15c4b58b150b2cd12498c6a04db91bc
androso <androso421@gmail.com>
2022-09-22 18:16:45 -0600

    adding /app endpoint

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,11 +1,10 @@
-
 export default function App() {
+	// if user is not logged in, redirect to /login
+	// if user is logged in, allow them access.
 	return (
 		<div>
 			<h1 className="text-xl text-red-800">App</h1>
-			<p>
-				This is a simple example of a React component.
-			</p>
+			<p>This is a simple example of a React component.</p>
 		</div>
 	);
 }
