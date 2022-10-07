a520680bd224646ed86d64abbd96930cfb7cb29a
Anibal Andrade <59257555+androso@users.noreply.github.com>
2022-10-07 23:44:20 +0000

    adding links to /index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-
-	
   return (
     <div className={styles.container}>
       <Head>
@@ -15,59 +13,16 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to Next.js on Replit!
+        <h1 className={`${styles.title} !mb-4`}>
+          Welcome to my Kanban app
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://docs.replit.com/category/hosting"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              This website automatically deployed on Replit by hitting the run button!
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="/__repl"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Built on
-          <span className={styles.logo}>
-            <Image src="/replit.svg" alt="Replit Logo" width={20} height={18} />
-          </span>
-          Replit
-        </a>
-      </footer>
+				<Link href="/login">
+					<a className="btn btn-primary mb-4">Sign in</a>
+				</Link>
+				<Link href="/login">
+					<a className="btn btn-secondary">Register</a>
+				</Link>
+			</main>
     </div>
   )
 }
