5721532715b1dbd4168ea94749fa72d6221b38d2
androso <androso421@gmail.com>
2022-10-09 15:51:23 -0600

    adding react-query to application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<Component {...pageProps} />
-			<Toaster />
+			<QueryClientProvider client={queryClient}>
+				<Component {...pageProps} />
+				<Toaster />
+			</QueryClientProvider>
 		</>
 	);
 }
