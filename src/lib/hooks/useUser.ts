966f3d8072f1aea4b05674b9db14c2052bc6a06e
androso <androso421@gmail.com>
2022-10-09 16:41:36 -0600

    adding hook that handles data from user logged in

---
+++ src/lib/hooks/useUser.ts
@@ -0,0 +1,42 @@
+import { User } from "../types";
+import { useQueryClient, useQuery } from "@tanstack/react-query";
+import { client } from "../helpers";
+
+const useUser = () => {
+	const queryClient = useQueryClient();
+
+	const {
+		data: user,
+		status,
+		error,
+	} = useQuery(
+		["user"],
+		async () => {
+			try {
+				const user = await client("/user");
+				return user as User;
+			} catch (e) {
+				return null;
+			}
+		},
+		{ retry: false, refetchOnWindowFocus: false }
+	);
+
+	const updateUser = (newUser: User) => {
+		queryClient.setQueryData(["user"], newUser);
+	};
+
+	const clearUser = () => {
+		queryClient.removeQueries(["user"]);
+	};
+
+	return {
+		user,
+		updateUser,
+		status,
+		error,
+		clearUser,
+	};
+};
+
+export default useUser;
--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from "react";
+import { client } from "../lib/helpers";
+import useUser from "../lib/hooks/useUser";
+
 export default function App() {
 	// if user is not logged in, redirect to /login
 	// if user is logged in, allow them access.
+
+	const { user } = useUser();
+
+	if (user) {
+		return (
+			<div>
+				<h1>hello {user.email}</h1>
+			</div>
+		);
+	}
 	return (
 		<div>
 			<h1 className="text-xl text-red-800">App</h1>
