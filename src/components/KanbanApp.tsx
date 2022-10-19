1804cb8892adb6112342ea1de3c9421349cbe7b3
androso <androso421@gmail.com>
2022-10-18 20:26:26 -0600

    Feature: deleting a status and all its related tasks

--- src/components/KanbanApp.tsx
+++ src/components/KanbanApp.tsx
@@ -9,6 +9,7 @@ import { client } from "../lib/helpers";
 import { useActiveBoardId } from "../lib/context/activeBoardId";
 import toast from "react-hot-toast";
 import { queryClient } from "../pages/_app";
+import { Icon } from "@iconify/react";
 
 function DroppableColumn({
 	children,
@@ -68,9 +69,9 @@ export default function KanbanApp() {
 	const [taskSelected, setTaskSelected] = useState<TaskFormatted>();
 	const [taskSelectedId, setTaskSelectedId] = useState<number>();
 	const [dialogCategory, setDialogCategory] = useState<
-		"add_status" | "task_details"
+		"add_status" | "task_details" | "delete_status"
 	>();
-
+	const [statusToDelete, setStatusToDelete] = useState<number>();
 	useEffect(() => {
 		if (taskSelectedId) {
 			const task = activeBoard?.tasks.find(
@@ -79,7 +80,19 @@ export default function KanbanApp() {
 			setTaskSelected(task);
 		}
 	}, [taskSelectedId, activeBoard]);
-
+	const deleteStatus = async () => {
+		try {
+			const result = await client(
+				`/user/boards/${activeBoard?.id}/statuses/${statusToDelete}`,
+				undefined,
+				"DELETE"
+			);
+			queryClient.invalidateQueries([ReactQueryQueries.ACTIVE_BOARD]);
+			closeDialog();
+		} catch (e) {
+			toast.error("Error deleting status");
+		}
+	};
 	return (
 		<section
 			id="kanban-container"
@@ -103,12 +116,26 @@ export default function KanbanApp() {
 								className="column grow-0 shrink-0 !basis-72 bg-base-200 mr-2 pb-1 rounded-t-md "
 								key={status.id}
 							>
-								<div className="column-header flex items-center  px-2 py-2 bg-neutral  rounded-md">
-									<span
-										className="w-4 h-4 rounded-full inline-block mr-3"
-										style={{ backgroundColor: status.color }}
-									></span>
-									<h2 className="font-semibold">{status.title}</h2>
+								<div className="column-header flex justify-between  px-2 py-2 bg-neutral  rounded-md">
+									<div className="flex items-center">
+										<span
+											className="w-4 h-4 rounded-full inline-block mr-3"
+											style={{ backgroundColor: status.color }}
+										></span>
+										<h2 className="font-semibold justify-self-start">
+											{status.title}
+										</h2>
+									</div>
+									<button
+										onClick={() => {
+											setDialogCategory("delete_status");
+											openDialog();
+											setStatusToDelete(status.id);
+										}}
+										className="btn-ghost text-lg p-1 rounded-md transition-colors hover:bg-error hover:text-primary-content"
+									>
+										<Icon icon="fe:trash" />
+									</button>
 								</div>
 								<DroppableColumn columnId={status.id}>
 									<div
@@ -162,8 +189,25 @@ export default function KanbanApp() {
 						task={taskSelected as TaskFormatted}
 						onClose={closeDialog}
 					/>
-				) : (
+				) : dialogCategory === "add_status" ? (
 					<AddStatus onClose={closeDialog} />
+				) : (
+					<div className=" prose">
+						<h4 className="mb-4">
+							Are you sure you want to delete this status and its task?
+						</h4>
+						<div className="flex justify-end">
+							<button className="btn btn-ghost mr-2" onClick={closeDialog}>
+								Cancel
+							</button>
+							<button
+								className={`btn btn-error ${status === "loading" && "loading"}`}
+								onClick={deleteStatus}
+							>
+								Delete
+							</button>
+						</div>
+					</div>
 				)}
 			</Dialog>
 		</section>
